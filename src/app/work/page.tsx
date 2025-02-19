9db94aaf9262edff1051df10bfb4904482af673a
fadilsiddique <fadilsiddique@gmail.com>
2025-02-19 14:05:12 +0400

    fix: ui update

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -7,7 +7,7 @@ import { person, work } from "@/app/resources/content";
 export async function generateMetadata() {
   const title = work.title;
   const description = work.description;
-  const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
+  // const ogImage = `https://${baseURL}/og?title=${encodeURIComponent(title)}`;
 
   return {
     title,
@@ -17,18 +17,18 @@ export async function generateMetadata() {
       description,
       type: "website",
       url: `https://${baseURL}/work/`,
-      images: [
-        {
-          url: ogImage,
-          alt: title,
-        },
-      ],
+      // images: [
+      //   {
+      //     url: ogImage,
+      //     alt: title,
+      //   },
+      // ],
     },
     twitter: {
       card: "summary_large_image",
       title,
       description,
-      images: [ogImage],
+      // images: [ogImage],
     },
   };
 }
